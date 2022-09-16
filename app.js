08de89fd6669ab5fbba83349352991a468fc0c69
Jean <jean-cabral@hotmail.com>
2022-09-16 12:50:37 -0300

    a

--- app.js
+++ app.js
@@ -51,8 +51,11 @@ app.post("/webhook", async (req, res) => { //i want some
             console.log('entrou no segundo if')
             let phon_no_id = body_param.entry[0].changes[0].value.metadata.phone_number_id;
             let from = body_param.entry[0].changes[0].value.messages[0].from;
-            let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+            let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body ? true : false;
 
+            if (!msg_body) {
+                msg_body = "sem body"
+            }
             console.log("phone number " + phon_no_id);
             console.log("from " + from);
             console.log("boady param " + msg_body);
@@ -188,4 +191,4 @@ async function sendMessageButton(number) {
     }
 };
 
-//sendMessageButton(number);
+sendMessageButton(number);
