fe26ed77d1a28e812e59996ccbaf78f3c4e17ed6
Jean <jean-cabral@hotmail.com>
2022-09-22 14:26:58 -0300

    asdkas

--- modules/class/ReceiveMessageConstructor.js
+++ modules/class/ReceiveMessageConstructor.js
@@ -1,6 +1,6 @@
 class ReceiveMessageConstructor {
   constructor({ msg_body, phon_no_id, from }) {
-    this.msg_body = msg_body || undefined;
+    this.msg_body = msg_body;
     // this.msg_interactive = msg_interactive || undefined;
     this.phon_no_id = phon_no_id;
     this.from = from;
